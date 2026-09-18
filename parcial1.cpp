bb6cdd7ad79a2bd1271d6c48aca8c3d4a6420ade
Andreaiszzz <00138324@uca.edu.sv>
2026-09-18 14:14:41 -0600

    feature:puntos de trayectoria

---
+++ parcial1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+struct PuntoTrayectoria {
+    int identificador;
+    int nombre;
+    float coordenada_x;
+    float coordenada_y;
+    float coordenada_z;
+};
+
+
+PuntoTrayectoria trayectoria[10];
+int main() {
+
+    return 0;
+}
