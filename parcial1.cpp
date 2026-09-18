00842b8267e6e94cc996eb3386e1058c3e195797
Andreaiszzz <00138324@uca.edu.sv>
2026-09-18 14:22:59 -0600

    feature/registro de puntos

--- parcial1.cpp
+++ parcial1.cpp
@@ -7,11 +7,18 @@ struct PuntoTrayectoria {
     float coordenada_x;
     float coordenada_y;
     float coordenada_z;
-};
-
-
+    };
 PuntoTrayectoria trayectoria[10];
+void registrarPuntos() {
+    for (int i = 0; i < 10; i++) {
+        trayectoria[i].identificador = i + 1;
+        trayectoria[i].nombre = i + 1;
+        trayectoria[i].coordenada_x = 0.0f;
+        trayectoria[i].coordenada_y = 0.0f;
+        trayectoria[i].coordenada_z = 0.0f;
+    }
+}
 int main() {
-
+    registrarPuntos();  
     return 0;
 }
